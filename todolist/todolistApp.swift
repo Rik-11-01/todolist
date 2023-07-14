9015598518d90739b2578392198f661950224f5c
Ritik Raman <ritikraman01@gmail.com>
2023-07-14 18:19:42 +0530

    Initial Commit

---
+++ todolist/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by Ritik Raman on 14/07/23.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
